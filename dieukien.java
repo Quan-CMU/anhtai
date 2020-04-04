e823558b2a1c7cd47927e8085856f299d1a3474c
Quan-CMU <63054576+Quan-CMU@users.noreply.github.com>
2020-04-04 09:34:33 +0700

    a = 0
    gr8-javaProject

---
+++ dieukien.java
@@ -0,0 +1,14 @@
+package thuchanh;
+
+public class dieukien {
+	float a,b;
+	void DieuKien() {
+		int a = 0,b = 0;
+		if (a==0)
+			System.out.println("vo nghiem!");
+		}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	dieukien s = new dieukien();
+}
+}
